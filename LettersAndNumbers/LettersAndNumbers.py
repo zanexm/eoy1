dcab66d4dc924247cfaf512753ae9685d530a55c
zanexm <zmen0027@student.monash.edu>
2024-12-08 12:31:09 +1100

    Created a new branch and imported text file containing 20k words

---
+++ LettersAndNumbers/LettersAndNumbers.py
@@ -0,0 +1,10 @@
+# Zane Mendes
+# Date Created: 8-12-2004
+# Last Modified: 8-12-2024
+# Version 1.0.0
+# This is a program that will execute function of a letters and numbers game (countdown)
+
+
+# Functionality
+# - Timer
+# - Solver (both letters and numbers)
---
+++ LettersAndNumbers/WordsToLetters.py
@@ -0,0 +1,5 @@
+# Zane Mendes
+# Date Created: 8-12-2004
+# Last Modified: 8-12-2024
+# Version 1.0.0
+# This program will convert words into letter forms of a seperate chained hash table. THis will be used to populate the words section for the letters and numbers game.
